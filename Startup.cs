ff8a1080586bd46ab8c1854fbd1d2809c37383b5
Vu Tien Hoa <hoavt184@gamil.com>
2021-09-26 15:12:10 +0700

    B3

--- Startup.cs
+++ Startup.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using App.Models;
 using App.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,11 @@ namespace App
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<AppDbContext>(options => {
+                string connectString = Configuration.GetConnectionString("AppMcvConnectionString");
+                options.UseSqlServer(connectString);
+            });
             services.AddControllersWithViews();
             services.AddRazorPages();
             // services.AddTransient
